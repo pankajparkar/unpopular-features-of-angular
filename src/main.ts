549803a060d0b1a3820aaf6d4cb4cf26776fb5f1
Pankaj Parkar <parkarpankaj3@gmail.com>
2023-04-28 18:03:14 +0800

    Added angular material

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
 import { provideRouter } from "@angular/router";
 import { routes } from "./app/routes";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes)
-  ],
+    provideRouter(routes),
+    importProvidersFrom(BrowserAnimationsModule)
+],
 });
